0addb362ce3655a9d417928e1627bc7560c56c09
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-10 16:01:27 -0600

    commit tree b39e7dc1b78057bf633fa003254c0756cb1d74e2

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
-# prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+    return nil if name_hash.empty?
+
+    values_array = name_hash.collect {|key, value| value}
+
+    smallest_value = values_array[0] 
+    values_array.each do |value| #compare each element to each other, starting with first
+        smallest_value = value if value < smallest_value     
+    end
+
+    name_hash.index(smallest_value)
 
 end
